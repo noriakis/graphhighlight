990b047e6f7ccb0d85722ab6a513622f7d5f4be8
noriakis <nori@hgc.jp>
2023-03-24 13:52:58 +0900

    update highlight_edge and highlight_node

--- R/highlight_edge.R
+++ R/highlight_edge.R
@@ -1,6 +1,8 @@
-highlight_edge <- function(edge_name, highlight_color="red") {
+
+highlight_edge <- function(edge_name, highlight_color="red", directed=TRUE) {
   structure(list(edge_name = edge_name,
-                 highlight_color = highlight_color),
+                 highlight_color = highlight_color,
+                 directed = directed),
             class = "highlight_edge")
 }
 
@@ -17,12 +19,25 @@ ggplot_add.highlight_edge <- function(object, plot, object_name) {
   if (is.vector(object$edge_name)) {
     frs <- object$edge_name[1]
     tos <- object$edge_name[2]
+  } else {
+    frs <- object$edge_name[,1]
+    tos <- object$edge_name[,2]
   }
   ed <- get_edges()(plot$data)
-  ed <- ed[ ed$node1.name %in% frs, ]
-  ed <- ed[ ed$node2.name %in% tos, ]
-  if (dim(ed)[1]==0) {stop("There is no edge specified")}
-  candidate_edge_id <- ed$edge.id
+  if (object$directed) {
+    ed <- ed[ ed$node1.name %in% frs, ]
+    ed <- ed[ ed$node2.name %in% tos, ]
+    candidate_edge_id <- ed$edge.id
+  } else {
+    candidate_edge_id <- NULL
+    for (i in seq_len(length(frs))) {
+      candidate_edge_id <- c(candidate_edge_id,
+       ed[ ed$node1.name %in% c(frs[i], tos[i]) & 
+             ed$node2.name %in% c(frs[i], tos[i]), ]$edge.id)
+    }
+  }
+  if (length(candidate_edge_id)==0) {stop("There is no edge specified")}
+  
   # build <- ggplot_build(plot)
   # re <- build$data[[1]]
   # re[ re$group %in% candidate_edge_id, "edge_colour" ] <- object$highlight_color
@@ -33,12 +48,17 @@ ggplot_add.highlight_edge <- function(object, plot, object_name) {
   for (l in seq_along(plot$layers)) {
     st <- plot$layers[[l]]$stat
     if (sum(grepl("StatEdge", attributes(st)$class))>0){
+      candl <- l
       cl <- attributes(st)$class
       se <- cl[which(sapply(cl, function(x) grepl("StatEdge",x)))]
     }
   }
   se <- tolower(se)
   geom <- paste0("geom_",substr(se, 5, 8),"_",substr(se, 9, nchar(se)))
-  plot + eval(parse(text = geom))(aes(filter=edge.id %in% candidate_edge_id),
-                            color=object$highlight_color)
+  aes_list <- plot$layers[[candl]]$mapping
+  ## Do not reflect to legends
+  plot + eval(parse(text = geom))(c(aes_list,
+                                    aes(filter=edge.id %in% candidate_edge_id)),
+                                  color=object$highlight_color,show.legend=FALSE)
+  
 }
--- R/highlight_node.R
+++ R/highlight_node.R
@@ -1,4 +1,3 @@
-
 highlight_node <- function(node_name, highlight_color="red") {
   structure(list(node_name = node_name,
                  highlight_color = highlight_color),
@@ -14,6 +13,17 @@ highlight_node <- function(node_name, highlight_color="red") {
 ggplot_add.highlight_node <- function(object, plot, object_name) {
   ## Or, directly modify ggplot_build(plot) to change color
   ## and re-convert gtable to ggplot2
-  plot + geom_node_point(aes(filter=.data$name %in% object$node_name),
-                         color=object$highlight_color)
+  candl <- NULL
+  for (l in seq_along(plot$layers)) {
+    st <- plot$layers[[l]]$geom
+    if (sum(grepl("GeomPoint", attributes(st)$class))>0){
+      candl <- l
+    }
+  }
+  if (is.null(candl)) {stop("No node_point geom")}
+  aes_list <- plot$layers[[candl]]$mapping
+  plot + geom_node_point(c(aes_list,
+                           aes(filter=.data$name %in% object$node_name)),
+                         color=object$highlight_color,
+                         show.legend=FALSE)
 }
