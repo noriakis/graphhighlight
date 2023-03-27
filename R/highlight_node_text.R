3fad147eb4d9f9d3507724ddbc82ded4344ce8cd
noriakis <nori@hgc.jp>
2023-03-27 16:27:55 +0900

    add highlight_node_text

---
+++ R/highlight_node_text.R
@@ -0,0 +1,92 @@
+#' highlight_node_text
+#' 
+#' This just stacks the layer changing the colour of 
+#' specified label in previous layer's output.
+#' Must specify seed to use with ggrepel, and should be 
+#' used with shadowtext in some environments.
+#' 
+#' @param node_name text labels to highlight
+#' @param filter text attributes to filter (in string)
+#' @param highlight_color specify highlighting colour
+#' @param seed make repel reproducible
+#' @export
+highlight_node_text <- function(node_name=NULL,
+                           highlight_color="red",
+                           filter=NULL,
+                           seed=42) {
+  structure(list(node_name = node_name,
+                 highlight_color = highlight_color,
+                 filter = filter,
+                 seed=seed
+                 ),
+            class = "highlight_node_text")
+}
+
+#' ggplot_add.highlight_node_text
+#' @param object An object to add to the plot
+#' @param plot The ggplot object to add object to
+#' @param object_name The name of the object to add
+#' @export ggplot_add.highlight_node
+#' @export
+ggplot_add.highlight_node_text <- function(object, plot, object_name) {
+
+  nd <- get_nodes()(plot$data)
+  if (!is.null(object$filter)) {
+    nd <- subset(nd, eval(parse(text=object$filter)))
+  }
+  filter_nodes <- nd$name
+  
+  if (!is.null(object$node_name)) {
+    nd <- nd[ nd$name %in% intersect(filter_nodes, object$node_name), ]
+  } else {
+    nd <- nd[ nd$name %in% filter_nodes, ]
+  }
+  
+  candl <- NULL
+  for (l in seq_along(plot$layers)) {
+    st <- plot$layers[[l]]$geom
+    if (sum(grepl("GeomText", attributes(st)$class))>0){
+      if (!is.null(candl)) {
+        message("multiple geom_node_text found, taking the last layer")
+      }
+      candl <- l
+      cl <- attributes(st)$class
+      se <- cl[which(sapply(cl, function(x) grepl("GeomText",x)))]
+      if (se=="GeomTextRepel") {
+        use_repel <- TRUE
+      } else {
+        use_repel <- FALSE
+      }
+    }
+  }
+  
+  if (is.null(candl)) {stop("No geom_node_text found")}
+  plot$layers[[candl]]$geom_params[["seed"]] <- object$seed
+  geom_param_list <- plot$layers[[candl]]$geom_params
+  print(geom_param_list)
+
+  if (is.null(object$highlight_color)) {
+    stop("Please specify highlight_color")
+  }
+
+  geom_param_list[["show.legend"]] <- FALSE
+  geom_param_list["na.rm"] <- NULL
+  
+  build <- ggplot_build(plot)$data[[candl]]
+  build[ build$label %in% nd$name, ]$colour <- object$highlight_color
+  aes_list <- plot$layers[[candl]]$mapping
+  geom_param_list[["repel"]] <- use_repel
+  if (use_repel) {
+    geom_param_list[["seed"]] <- object$seed
+  }
+  geom_param_list["color"] <- NULL;
+  
+  # TODO: glowing function, but shadowtext is superior to highlighting.
+  # Obtain repelled coordinates to support this option.
+
+  # Just highlight (stack the layer)
+  plot + do.call(geom_node_text,
+                 c(geom_param_list, list(
+                   label=build$label,
+                    color=build$colour)))
+}
--- README.md
+++ README.md
@@ -19,7 +19,7 @@ ggraph(g, layout="nicely")+
   geom_edge_diagonal(aes(width=.data$weight,
                          color=.data$weight))+
   geom_node_point(aes(size=size))+
-  geom_node_text(aes(label=name), repel=TRUE)+
+  geom_node_text(aes(label=name), repel=TRUE, color="yellow", bg.colour="grey80")+
   scale_edge_color_gradient(low="grey",high="red")+
   scale_edge_width(range=c(0.1,1))+
   highlight_edge(filter="weight>8",highlight_color = "blue")+
@@ -27,7 +27,7 @@ ggraph(g, layout="nicely")+
   theme_graph()
 ```
 
-<img src="man/figures/README-unnamed-chunk-1-1.png" width="768" style="display: block; margin: auto;" />
+<img src="man/figures/README-unnamed-chunk-1-1.png" width="2400" style="display: block; margin: auto;" />
 
 ``` r
 ggraph(g, layout="nicely")+
@@ -44,7 +44,7 @@ ggraph(g, layout="nicely")+
   theme_graph()
 ```
 
-<img src="man/figures/README-unnamed-chunk-2-1.png" width="768" style="display: block; margin: auto;" />
+<img src="man/figures/README-unnamed-chunk-2-1.png" width="2400" style="display: block; margin: auto;" />
 
 ``` r
 ggraph(g, layout="nicely")+
@@ -63,4 +63,4 @@ ggraph(g, layout="nicely")+
   theme_graph()
 ```
 
-<img src="man/figures/README-unnamed-chunk-3-1.png" width="768" style="display: block; margin: auto;" />
+<img src="man/figures/README-unnamed-chunk-3-1.png" width="2400" style="display: block; margin: auto;" />
