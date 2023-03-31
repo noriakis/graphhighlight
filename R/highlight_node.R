637894770b1ab6e633a9910d6df41ea7267e605c
noriakis <nori@hgc.jp>
2023-03-31 11:54:58 +0900

    delete unnecessary arguments

--- R/highlight_node.R
+++ R/highlight_node.R
@@ -5,16 +5,15 @@
 #' @param highlight_color highlighting color
 #' @param filter used to subset node data specified as string
 #' @param glow use glow highlighting
-#' @param glow_fixed_color use glow highlighting with fixed color,
-#' specified by `highlight_color` (default: FALSE, use the raw color of the nodes)
 #' @param glow_base_size use base size of the node for initial size of glowing
-#' @param highlight_by_shape boolean, use shape highlighting
 #' @param shape_number shape number
 #' @param shape_size shape size
 #' @param shape_color shape color 
 #' @param shape_with_text if specified, show text along with shape (21 only)
 #' using `geomtextpath`
 #' @param with_text if specified, show text around the nodes, not the shape
+#' @param text_attribute if not NULL, graph attributes of this name will be used as text annotation
+#' @param text_node_color if TRUE, node color will be used as text color
 #' @param glow_size argument to control how big the glowing will be
 #' @param override_text after highlighting, stack the last geom_node_text layer
 #' @param use_ggfx use ggfx geom to highlight, default to NULL
@@ -28,14 +27,14 @@ highlight_node <- function(node_name=NULL,
                            highlight_color=NULL,
                            filter=NULL,
                            glow=FALSE,
-                           glow_fixed_color=FALSE,
                            glow_base_size=FALSE,
-                           highlight_by_shape=FALSE,
                            shape_number=NULL,
                            shape_size=NULL,
                            shape_color=NULL,
                            shape_with_text=NULL,
                            with_text=NULL,
+                           text_attribute=NULL,
+                           text_node_color=FALSE,
                            glow_size=1.2,
                            override_text=FALSE,
                            use_ggfx=NULL,
@@ -45,15 +44,15 @@ highlight_node <- function(node_name=NULL,
                  highlight_color = highlight_color,
                  filter = filter,
                  glow = glow,
-                 glow_fixed_color = glow_fixed_color,
                  glow_base_size = glow_base_size,
                  glow_size=glow_size,
-                 highlight_by_shape = highlight_by_shape,
                  shape_number = shape_number,
                  shape_size = shape_size,
                  shape_color = shape_color,
                  shape_with_text = shape_with_text,
                  with_text = with_text,
+                 text_attribute=text_attribute,
+                 text_node_color=text_node_color,
                  override_text = override_text,
                  use_ggfx = use_ggfx,
                  ggfx_params = ggfx_params,
@@ -123,13 +122,14 @@ ggplot_add.highlight_node <- function(object, plot, object_name) {
   } else {
     if (object$glow) {
       plot <- glow_nodes(plot, aes_list, candidate_node_id, geom_param_list, object$glow_base_size, candl,
-        object$glow_fixed_color, object$highlight_color, object$glow_size)  
+        object$highlight_color, object$glow_size)  
     } else {
       if (!is.null(object$with_text)) {
           plot <- append_textpath(plot=plot, candl=candl, candidate_node_id=candidate_node_id,
-            shape_with_text=object$with_text, along_with="node", textpath_params=object$textpath_params)
+            shape_with_text=object$with_text, along_with="node", textpath_params=object$textpath_params,
+            text_attribute=object$text_attribute, text_node_color=object$text_node_color)
       } else {
-        if (object$highlight_by_shape) {
+        if (!is.null(object$shape_number)) {
           if (is.null(object$shape_number)) {stop("Please specify shape")}
           if (is.null(object$shape_size)) {stop("Please specify shape size")}
           geom_param_list[["shape"]] <- object$shape_number
@@ -143,7 +143,10 @@ ggplot_add.highlight_node <- function(object, plot, object_name) {
                                            geom_param_list))
           if (!is.null(object$shape_with_text)) {
             plot <- append_textpath(plot=plot, candl=candl, candidate_node_id=candidate_node_id,
-              shape_with_text=object$shape_with_text, along_with="shape", textpath_params=object$textpath_params)
+              shape_with_text=object$shape_with_text, along_with="shape",
+              textpath_params=object$textpath_params,
+              text_attribute=object$text_attribute,
+              text_node_color=object$text_node_color)
           }
         } else {
           plot <- plot + do.call(geom_node_point,c(list(mapping=c(aes_list,
@@ -164,7 +167,7 @@ ggplot_add.highlight_node <- function(object, plot, object_name) {
 #' @noRd
 glow_nodes <- function(plot, aes_list, candidate_node_id,
                        geom_param_list, glow_base_size, candl,
-                       glow_fixed_color, highlight_color, glow_size) {
+                       highlight_color, glow_size) {
   layers <- 10
   size <- 8
   aes_list[["fill"]] <- NA
@@ -172,7 +175,7 @@ glow_nodes <- function(plot, aes_list, candidate_node_id,
   aes_list[["alpha"]] <- 1
   aes_list["size"] <- NULL
 
-  if (glow_fixed_color) {
+  if (!is.null(highlight_color)) {
     aes_list["color"] <- NULL
     geom_param_list[["color"]] <- highlight_color
   }
@@ -197,7 +200,8 @@ glow_nodes <- function(plot, aes_list, candidate_node_id,
 
 #' @noRd
 append_textpath <- function(plot, candl, candidate_node_id,
-            shape_with_text, along_with, textpath_params) {
+            shape_with_text, along_with, textpath_params,
+            text_attribute, text_node_color) {
   build <- ggplot_build(plot)$data
   if (along_with=="shape") {
     ## Take the last layer in which the shape has been added
@@ -215,8 +219,15 @@ append_textpath <- function(plot, candl, candidate_node_id,
                       sin(t)*build[row,"ssize"],
                     y=as.numeric(build[row,"y"]) + 
                       cos(t)*build[row,"ssize"],
-                    text=shape_with_text,
-                    group=row
+                    text=ifelse(is.null(text_attribute),
+                      shape_with_text,
+                      plot$data[plot$data$.ggraph.orig_index %in% candidate_node_id,]
+                      [row, text_attribute]),
+                    group=row,
+                    color=ifelse(text_node_color,
+                      build[row, "colour"],
+                      ifelse(!is.null(textpath_params[["color"]]),
+                        textpath_params[["color"]], "black"))
                     )
                   },
       simplify=FALSE)) |> data.frame()
@@ -226,6 +237,7 @@ append_textpath <- function(plot, candl, candidate_node_id,
   textpath_params[["group"]] <- pos$group
   textpath_params[["label"]] <- pos$text
   textpath_params[["data"]] <- pos
+  textpath_params[["color"]] <- pos$color
 
   plot <- plot + do.call(geom_textpath, textpath_params)
   plot
